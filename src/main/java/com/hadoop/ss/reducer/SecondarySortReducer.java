ae1a531b748f2e7b4cf5c0152495707db57f9559
Ritesh Sinha <kumarris@usc.edu>
2018-03-27 00:28:36 -0700

    added reducer class

--- src/main/java/com/hadoop/ss/reducer/SecondarySortReducer.java
+++ src/main/java/com/hadoop/ss/reducer/SecondarySortReducer.java
@@ -1,4 +1,20 @@
 package com.hadoop.ss.reducer;
 
-public class SecondarySortReducer {
+import com.hadoop.ss.entity.PersonEntity;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class SecondarySortReducer extends Reducer<PersonEntity, Text, Text, Text> {
+    private static final Logger LOG = LoggerFactory.getLogger(SecondarySortReducer.class);
+
+    public void reduce(PersonEntity key, Iterable<Text> value, Context context) throws IOException, InterruptedException {
+        for (Text firstName : value) {
+            context.write(new Text(key.getLastName()), new Text(firstName));
+            LOG.info("Data emitted by reducer : {} , {}", key.getLastName(), firstName);
+        }
+    }
 }
