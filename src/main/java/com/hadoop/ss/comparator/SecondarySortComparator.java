e2057f86395bbcb79bafbf1dd62e3e024c924c74
Ritesh Sinha <kumarris@usc.edu>
2018-03-27 01:12:42 -0700

    comparator to sort data according to lastname followed by firstname

--- src/main/java/com/hadoop/ss/comparator/SecondarySortComparator.java
+++ src/main/java/com/hadoop/ss/comparator/SecondarySortComparator.java
@@ -1,4 +1,22 @@
 package com.hadoop.ss.comparator;
 
-public class SecondarySortComparator {
+import com.hadoop.ss.entity.PersonEntity;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+
+public class SecondarySortComparator extends WritableComparator {
+
+    public SecondarySortComparator() {
+        super(PersonEntity.class, true);
+    }
+
+    public int compare(WritableComparable o1, WritableComparable o2) {
+        PersonEntity p1 = (PersonEntity) o1;
+        PersonEntity p2 = (PersonEntity) o2;
+        int compare = p1.getLastName().compareTo(p2.getLastName());
+        if (compare == 0)
+            compare = p1.getFirstName().compareTo(p2.getFirstName());
+        return compare;
+    }
 }
