d14525189aa4b06e4e9e55faa0e476591f6f9332
Ritesh Sinha <kumarris@usc.edu>
2018-03-27 00:16:08 -0700

    added mapper class

--- src/main/java/com/hadoop/ss/mapper/SecondarySortMapper.java
+++ src/main/java/com/hadoop/ss/mapper/SecondarySortMapper.java
@@ -1,4 +1,28 @@
 package com.hadoop.ss.mapper;
 
-public class SecondarySortMapper {
+import com.hadoop.ss.entity.PersonEntity;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class SecondarySortMapper extends Mapper<LongWritable, Text, PersonEntity, Text> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SecondarySortMapper.class);
+
+    /**
+     * @param key
+     * @param value
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] personValue = value.toString().split(",");
+        context.write(new PersonEntity(personValue[0], personValue[1]), new Text(personValue[0]));
+        LOG.info("Data emitted from mapper : {} , {}", personValue[0], personValue[1]);
+    }
 }
