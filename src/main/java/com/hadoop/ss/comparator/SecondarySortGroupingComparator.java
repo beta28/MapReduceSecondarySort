78ff0e8274812010922167bc956854dfc57430bb
Ritesh Sinha <kumarris@usc.edu>
2018-03-27 20:18:42 -0700

    grouping comparator class

--- src/main/java/com/hadoop/ss/comparator/SecondarySortGroupingComparator.java
+++ src/main/java/com/hadoop/ss/comparator/SecondarySortGroupingComparator.java
@@ -1,4 +1,18 @@
 package com.hadoop.ss.comparator;
 
-public class SecondarySortGroupingComparator {
+import com.hadoop.ss.entity.PersonEntity;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class SecondarySortGroupingComparator extends WritableComparator {
+
+    public SecondarySortGroupingComparator() {
+        super(PersonEntity.class, true);
+    }
+
+    public int compare(WritableComparable o1, WritableComparable o2) {
+        PersonEntity p1 = (PersonEntity) o1;
+        PersonEntity p2 = (PersonEntity) o2;
+        return p1.getLastName().compareTo(p2.getLastName());
+    }
 }
